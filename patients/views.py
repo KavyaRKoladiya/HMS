18a101e865d54e970c5786d93803695451b282f8
Mahi Bhatti <25ceubg902@ddu.ac.in>
2026-02-12 12:01:09 +0530

    Implemented patient create form with ModelForm

--- patients/views.py
+++ patients/views.py
@@ -1,9 +1,20 @@
 
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Patient
+from .forms import PatientForm
 
 def patient_list(request):
     patients = Patient.objects.all()
     return render(request, 'patients/patient_list.html', {'patients': patients})
+
+
+def patient_create(request):
+    if request.method == 'POST':
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('patient_list')
+    else:
+        form = PatientForm()
+
+    return render(request, 'patients/patient_form.html', {'form': form})
