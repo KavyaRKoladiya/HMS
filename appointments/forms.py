358a47ad871fb777d718f5505a3db96e244a36c9
Kavya K <kkoladiya1596@gmail.com>
2026-02-15 17:17:31 +0530

    Enforced validation to prevent creation of past appointments

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Appointment
+from django.utils import timezone
 
 class AppointmentForm(forms.ModelForm):
     class Meta:
@@ -8,3 +9,11 @@ class AppointmentForm(forms.ModelForm):
         widgets = {
             'appointment_date': forms.DateTimeInput(attrs={'type': 'datetime-local'})
         }
+
+    def clean_appointment_date(self):
+            appointment_date = self.cleaned_data.get('appointment_date')
+
+            if appointment_date < timezone.now():
+                raise forms.ValidationError("You cannot create an appointment in the past.")
+
+            return appointment_date
